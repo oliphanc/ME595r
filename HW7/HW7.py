b43045977a1c62959e5f755245f989eef618b712
Your Name <chase.oliphant@live.com>
2025-02-25 15:07:13 -0700

    .

--- HW7/HW7.py
+++ HW7/HW7.py
@@ -17,7 +17,7 @@ class DeepKoopman(nn.Module):
         self.encoder = nn.Sequential(*layers)
 
         self.K = nn.Linear(layer_widths[-1], layer_widths[-1], bias=False)
-        nn.init.normal_(self.K.weight, 0, 0.01)
+        nn.init.normal_(self.K.weight, 0, 0.0001)
         
         layers = []
         for i in range(n-1, 0, -1):
@@ -33,16 +33,16 @@ class DeepKoopman(nn.Module):
         return xk_1
 
 
-def train(model, Ytrain, Ytest, epochs=600, autoencoder_epochs=100, lr=5e-3):
+def train(model, Ytrain, Ytest, epochs=3000, autoencoder_epochs=100, lr=1e-3):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     loss_func = nn.MSELoss()
     
     train_losses = []
-    test_losses = [1]
+    test_losses = []
     torch.autograd.set_detect_anomaly(True)
     iters = tqdm(range(epochs))
-    max_timestep = 10
+    max_timestep = 5
 
     for epoch in iters:
         model.train()
@@ -53,32 +53,29 @@ def train(model, Ytrain, Ytest, epochs=600, autoencoder_epochs=100, lr=5e-3):
 
         if epoch > autoencoder_epochs:
 
-            if epoch % 100 == 0:
-                max_timestep += 10
+            if epoch % 300 == 0:
+                max_timestep += 5
                 tqdm.write(f"Increasing max timestep to {max_timestep}")
+                # break
 
 
             latent_prediction = torch.zeros(encoding.shape[0], 1, encoding.shape[2], dtype=torch.float64).to(device)
             latent_prediction[:, 0, :] = encoding[:, 0, :]
-            prediction = torch.zeros(Ytrain.shape[0], 1, Ytrain.shape[2], dtype=torch.float64).to(device)
-            prediction[:, 0, :] = Ytrain[:, 0, :]
             
             if max_timestep >= 50:
-                max_timestep=48
+                max_timestep=49
 
             for t in range(max_timestep):
                 new_latent = model.K(latent_prediction[:, -1, :])
-                # tqdm.write(str(new_latent.shape))
-                
                 latent_prediction = torch.concatenate((latent_prediction, new_latent.unsqueeze(1)), dim=1)
-                new_pred = model(prediction[:, -1, :])
-                prediction = torch.concatenate((prediction, new_pred.unsqueeze(1)), dim=1)
+            
+            prediction = model.decoder(latent_prediction)
             
             # Linear dynamics
-            linear_loss = loss_func(encoding[:, 1:max_timestep+1, :], latent_prediction[:, :-1, :])
+            linear_loss = loss_func(encoding[:, 1:max_timestep, :], latent_prediction[:, 1:-1, :])
 
             # State Prediction
-            prediction_loss = loss_func(Ytrain[:, 1:max_timestep+1, :], prediction[:, :-1, :])
+            prediction_loss = loss_func(Ytrain[:, 1:max_timestep, :], prediction[:, 1:-1, :])
 
             total_loss += linear_loss
             total_loss += prediction_loss
@@ -88,40 +85,40 @@ def train(model, Ytrain, Ytest, epochs=600, autoencoder_epochs=100, lr=5e-3):
         optimizer.step()
         train_losses.append(total_loss.item())
 
-        # model.eval()
-        # with torch.no_grad():
-        #     test_encoding = model.encoder(Ytrain)
-        #     test_loss = loss_func(Ytrain, model.decoder(test_encoding))
-        #     if epoch > autoencoder_epochs:
+        model.eval()
+        with torch.no_grad():
+            test_encoding = model.encoder(Ytrain)
+            test_loss = loss_func(Ytrain, model.decoder(test_encoding))
+            if epoch > autoencoder_epochs:
                 
-        #         test_latent_prediction = torch.zeros_like(test_encoding)
-        #         test_latent_prediction[:, 0, :] = test_encoding[:, 0, :]
-        #         test_prediction = torch.zeros_like(Ytrain)
-        #         test_prediction[:, 0, :] = Ytrain[:, 0, :]
+                test_latent_prediction = torch.zeros_like(test_encoding)
+                test_latent_prediction[:, 0, :] = test_encoding[:, 0, :]
                 
-        #         for t in range(1, test_prediction.shape[1]):
-        #             test_latent_prediction[:, t, :] = model.K(test_encoding[:, t-1, :])
-        #             test_prediction[:, t, :] = model(test_prediction[:, t-1, :])
+                for t in range(1, Ytest.shape[1]-1):
+                    test_latent_prediction[:, t, :] = model.K(test_encoding[:, t-1, :])
+                    
+                test_prediction = model.decoder(test_latent_prediction)
 
-        #         # Linear dynamics
-        #         test_linear_loss = loss_func(test_encoding[:, 1:, :], test_latent_prediction[:, :-1, :])
+                # Linear dynamics
+                test_linear_loss = loss_func(test_encoding[:, 1:, :], test_latent_prediction[:, 1:, :])
 
-        #         # State Prediction
-        #         test_prediction_loss = loss_func(Ytrain[:, 1:, :], test_prediction[:, :-1, :])
+                # State Prediction
+                test_prediction_loss = loss_func(Ytrain[:, 1:, :], test_prediction[:, 1:, :])
 
-        #         test_loss += test_linear_loss
-        #         test_loss += test_prediction_loss
+                test_loss += test_linear_loss
+                test_loss += test_prediction_loss
             
-        #     test_losses.append(test_loss.item())
+            test_losses.append(test_loss.item())
 
         iters.set_description(f"Losses: ({train_losses[-1]:.5f}, {test_losses[-1]:.5f})")
 
     plt.figure()
     plt.plot(train_losses)
-    # plt.plot(test_losses)
+    plt.plot(test_losses)
     plt.yscale('log')
     plt.xlabel('Epochs')
     plt.ylabel('Total Loss')
+    plt.savefig(r"HW7\trainingloss.png")
     plt.show()
 
 
@@ -133,25 +130,26 @@ if __name__ == "__main__":
     ntraj = 2148  # number of trajectories
     nt = 50  # number of time steps
     ny = 7  # number of states
-    nz = 32 # number of dimensions in latent space
+    nz = 64 # number of dimensions in latent space
 
     tvec = np.linspace(0, 350, nt)
     Y = np.loadtxt(r'data\kdata.txt').reshape(ntraj, nt, ny)
-    Ytrain = Y[:1, :, :]  # 2048 training trajectories
+    Ytrain = Y[:5, :, :]  # 2048 training trajectories
     Ytest = Y[2048:, :, :]  # 100 testing trajectoreis
 
-    encoder_layers = [ny, 256, 256, nz]
+    encoder_layers = [ny, 256, 256, 128, nz]
 
     model = DeepKoopman(encoder_layers).double().to(device)
     Ytrain_torch = torch.tensor(Ytrain, dtype=torch.float64).to(device)
     Ytest_torch  = torch.tensor(Ytest, dtype=torch.float64).to(device)
-
+    model.load_state_dict(torch.load(r"HW7\model.pt", weights_only=True))
     train(model, Ytrain_torch, Ytest_torch)
+    torch.save(model.state_dict(), r"HW7/model.pt")
     prediction = torch.zeros_like(Ytrain_torch[0], dtype=torch.float64).to(device)
     prediction[0] = Ytrain_torch[0][0]
-
-    for t in range(1, prediction.shape[0]):
-        prediction[t] = model(prediction[t-1])
+    with torch.no_grad():
+        for t in range(1, nt):
+            prediction[t] = model(prediction[t-1])
 
     prediction_np = prediction.cpu().detach().numpy()
     plot_case = Y[0]
@@ -165,4 +163,26 @@ if __name__ == "__main__":
     plt.plot(prediction_np[:,1], 'g--')
     plt.plot(prediction_np[:,2], 'b--')
     plt.ylim(0, 2.5)
+    plt.savefig(r"HW7\stateprediction.png")
+    plt.show()
+
+    prediction = torch.zeros_like(Ytest_torch[0], dtype=torch.float64).to(device)
+    prediction[0] = Ytest_torch[0][0]
+    with torch.no_grad():
+        for t in range(1, nt):
+            prediction[t] = model(prediction[t-1])
+
+    prediction_np = prediction.cpu().detach().numpy()
+    plot_case = Y[0]
+
+    plt.figure()
+    plt.plot(plot_case[:,0], 'r-')
+    plt.plot(plot_case[:,1], 'g-')
+    plt.plot(plot_case[:,2], 'b-')
+
+    plt.plot(prediction_np[:,0], 'r--')
+    plt.plot(prediction_np[:,1], 'g--')
+    plt.plot(prediction_np[:,2], 'b--')
+    plt.ylim(0, 2.5)
+    plt.savefig(r"HW7\stateprediction.png")
     plt.show()
